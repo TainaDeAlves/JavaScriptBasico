8d5440eea340bfa1cdcc149cf8327ab5f6828b34
Taina Souza <tainadesouza0001@gmail.com>
2023-11-14 21:52:28 -0300

    validação do cpf do digito X

--- 4-formulario/js/validar-cpf.js
+++ 4-formulario/js/validar-cpf.js
@@ -1,16 +1,43 @@
 document.getElementById("validarbutton").
-addEventListener("click",function(){
-    // alert("deu certo")
-    const cpf=document.getElementById("cpfinput").value;
+    addEventListener("click", function () {
+        // alert("deu certo")
+        const cpf = document.getElementById("cpfinput").value;
 
-    if(validacpf(cpf)){
-        document.getElementById("resultado").textContent="CPF Valido"
-    }
-    else{
-        document.getElementById("resultado").textContent="CPF invalido"
-    }
-})
-function validacpf(vrcpf){
-    return vrcpf.length ===11 & !isNaN(vrcpf);
+        if (validacpf(cpf)) {
+            document.getElementById("resultado").textContent = "CPF Valido"
+        }
+        else {
+            document.getElementById("resultado").textContent = "CPF invalido"
+        }
+    })
+function validacpf(vrcpf) {
+    let soma = 0;
+    soma += parseInt(vrcpf.charAt(0)) * 10;
+    soma += parseInt(vrcpf.charAt(1)) * 9;
+    soma += parseInt(vrcpf.charAt(2)) * 8;
+    soma += parseInt(vrcpf.charAt(3)) * 7;
+    soma += parseInt(vrcpf.charAt(4)) * 6;
+    soma += parseInt(vrcpf.charAt(5)) * 5;
+    soma += parseInt(vrcpf.charAt(6)) * 4;
+    soma += parseInt(vrcpf.charAt(7)) * 3;
+    soma += parseInt(vrcpf.charAt(8)) * 2;
 
+    let digitoX = soma%11;
+    digitoX = 11 - digitoX;
+
+    if (digitoX > 10) {
+        digitoX = 0;
+    }
+
+    let digitValido = true;
+
+    if (digitoX !== parseInt(vrcpf.charAt(9))) {
+        digitValido = false;
+    }
+    
+
+    return digitValido;
+
+
+    return vrcpf.length === 11 & !isNaN(vrcpf);
 }
